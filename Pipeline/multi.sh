3ec4b5e1e90c970a01a988641f1b1aabcaf5a702
Mohammed Gasmallah <mhg1@gra-login3.graham.sharcnet>
2018-09-08 17:07:39 -0400

    Removed

--- Pipeline/multi.sh
+++
@@ -1,1 +0,0 @@
-python -c "from multi_index import *;ppd=PreProcessData('./OpenImage.txt');ppd.get_open_images('../../Dataset/OpenImage/',data_type='val')"
--- Pipeline/test_data.sh
+++
@@ -1,1 +0,0 @@
-python -c "from test_data2 import *;ppd=PreProcessData('./OpenImage.txt');ppd.get_open_images('../../Dataset/OpenImage/',data_type='train')"
