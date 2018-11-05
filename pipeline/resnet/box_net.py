9ecac896d7e41bdcf35809b63d83c58a388f57d6
Mohammed Gasmallah <mhg1@gra-login3.graham.sharcnet>
2018-11-04 21:07:08 -0500

    Made the graph smaller by adding a handler instead of messing around with two versions of the graph

--- pipeline/resnet/box_net.py
+++ pipeline/resnet/box_net.py
@@ -28,30 +28,31 @@ class Box_net():
         data = Data(classification=False, classes_text=self.flags.labels, batch_size = self.flags.batch_size)
         self.train_dataset = data.get_batch(self.flags.data_dir)
         self.val_dataset = data.get_batch(self.flags.val_dir)
+        #create handle for switching iterators
+        self.handle = tf.placeholder(tf.string,shape=[])
+        self.iterator = tf.data.Iterator.from_string_handle(
+                self.handle,self.train_dataset.output_types,self.train_dataset.output_shapes)
+        self.image, self.label, self.box = self.iterator.get_next()
         self.train_iter = self.train_dataset.make_initializable_iterator()
         self.val_iter = self.val_dataset.make_initializable_iterator()
-
-        self.train_image, self.train_label, self.train_box = self.train_iter.get_next()
-        self.val_image, self.val_label, self.val_box = self.val_iter.get_next()
-
+        
+        
         #put labels in -1 to 1 range
-        t_xy = (self.train_box[:,:2] * 2.0) - 1.0
-        t_wh = tf.log(self.train_box[:,2:])
-        self.train_box = tf.concat([t_xy,t_wh],-1)
-
-        v_xy = (self.val_box[:,:2] * 2.0) - 1.0
-        v_wh = tf.log(self.val_box[:,2:])
-        self.val_box = tf.concat([v_xy, v_wh],-1)
+        t_xy = (self.box[:,:2] * 2.0) - 1.0
+        t_wh = tf.log(self.box[:,2:])
+        self.box = tf.concat([t_xy,t_wh],-1)
 
     def train_step(self, sess, train_op, metrics_op, global_step):
         start_time = time.time()
-        total_loss, global_step_count, _ = sess.run([train_op, global_step, metrics_op])
+        total_loss, global_step_count, _ = sess.run([train_op, global_step, metrics_op],
+                feed_dict={self.training:True,self.handle:self.training_handle})
         time_elapsed = time.time() - start_time
         logging.info('global_step %s: loss %.4f (%.2f sec/step)', global_step_count, total_loss, time_elapsed)
         return total_loss, global_step_count
 
     def val_step(self, sess, val_op, val_metric):
-        v_loss, v_acc = sess.run([val_op, val_metric])
+        v_loss, v_acc = sess.run([val_op, val_metric],
+                feed_dict={self.training:False,self.handle:self.validation_handle})
         return v_loss, v_acc
 
     def train(self):
@@ -60,18 +61,14 @@ class Box_net():
         decay_steps = int(self.num_epochs_before_decay * num_steps_per_epoch)
 
         if self.dtype == 'float32':
-            self.train_image = tf.cast(self.train_image,tf.float32)
-            self.val_image = tf.cast(self.val_image,tf.float32)
-        else:
-            self.train_image = tf.cast(self.train_image,tf.float16)
-            self.val_image = tf.cast(self.val_image,tf.float16)
+            self.image = tf.cast(self.image,tf.float32)
+        self.training = tf.placeholder(tf.bool,name="training")
+        out, attn = get_box_resnet(self.image,is_training=self.training)
 
-        train_out, train_attn = get_box_resnet(self.train_image,is_training=True)
-        
         if self.dtype == 'float16':
-            train_out = tf.cast(train_out, tf.float32)
+            out = tf.cast(out, tf.float32)
 
-        loss = tf.losses.mean_squared_error(self.train_box,train_out)
+        loss = tf.losses.mean_squared_error(self.box,out)
         total_loss = tf.losses.get_total_loss()
         
         global_step = tf.train.get_or_create_global_step()
@@ -83,24 +80,24 @@ class Box_net():
                 decay_rate = self.lr_decay,
                 staircase=True)
 
-#        loss_scale = 128. if self.dtype=='float16' else 1.
-#
-#        variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
-#        grad_mult = {variables[i]:1./loss_scale for i in range(0,len(variables))}
+        loss_scale = 128. if self.dtype=='float16' else 1.
+
+        variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+        grad_mult = {variables[i]:1./loss_scale for i in range(0,len(variables))}
 
         optimizer = tf.train.AdamOptimizer(learning_rate=lr)
-#        train_op = slim.learning.create_train_op(loss * loss_scale, optimizer, gradient_multipliers=grad_mult)
-        train_op = slim.learning.create_train_op(total_loss, optimizer)
+        train_op = slim.learning.create_train_op(loss * loss_scale, optimizer, gradient_multipliers=grad_mult)
 
 
-        rmse, rmse_update = tf.metrics.root_mean_squared_error(self.train_box,train_out) 
+
+        rmse, rmse_update = tf.metrics.root_mean_squared_error(self.box,out) 
         metrics_op = tf.group(rmse_update)
 
 
         #train summaries
-        train_image_summ = tf.summary.image('train/input_images',self.train_image[:5,:,:,:3])
-        train_mask_summ = tf.summary.image('train/input_masks',self.train_image[:5,:,:,3:])
-        train_attn_summ = tf.summary.image('train/attn_out',train_attn[:5,:,:,:])
+        train_image_summ = tf.summary.image('train/input_images',self.image[:5,:,:,:3])
+        train_mask_summ = tf.summary.image('train/input_masks',self.image[:5,:,:,3:])
+        train_attn_summ = tf.summary.image('train/attn_out',attn[:5,:,:,:])
         train_loss_summ = tf.summary.scalar('train/train_loss',total_loss)
         train_rmse_summ = tf.summary.scalar('train/rmse',rmse)
         train_lr_summ = tf.summary.scalar('train/learning_rate',lr)
@@ -108,24 +105,11 @@ class Box_net():
         train_summary_op = tf.summary.merge([train_image_summ,train_attn_summ,
             train_mask_summ,train_loss_summ,train_rmse_summ,train_lr_summ])
 
-        #validation
-        val_out, val_attn = get_box_resnet(self.val_image,is_training=False)
-
-        if self.dtype=='float16':
-            val_out = tf.cast(val_out,tf.float32)
-
-        val_loss = tf.sqrt(tf.reduce_sum(tf.square(self.val_box - val_out)))
-
-        val_rmse, val_rmse_update = tf.metrics.root_mean_squared_error(self.val_box, val_out)
-
-        val_metrics_op = tf.group(val_rmse_update)
-
-        #summaries
-        val_image_summ = tf.summary.image('val/input_images',self.val_image[:5,:,:,:3])
-        val_mask_summ = tf.summary.image('val/input_masks',self.val_image[:5,:,:,3:])
-        val_attn_summ = tf.summary.image('val/attn_out',val_attn[:5,:,:,:])
-        val_loss_summ = tf.summary.scalar('val/train_loss',val_loss)
-        val_acc_summ = tf.summary.scalar('val/rmse',val_rmse)
+        val_image_summ = tf.summary.image('val/input_images',self.image[:5,:,:,:3])
+        val_mask_summ = tf.summary.image('val/input_masks',self.image[:5,:,:,3:])
+        val_attn_summ = tf.summary.image('val/attn_out',attn[:5,:,:,:])
+        val_loss_summ = tf.summary.scalar('val/train_loss',loss)
+        val_acc_summ = tf.summary.scalar('val/rmse',rmse)
 
         val_summary_op = tf.summary.merge([val_image_summ, val_mask_summ,
             val_attn_summ,val_loss_summ,val_acc_summ])
@@ -135,8 +119,12 @@ class Box_net():
         
         config = tf.ConfigProto(allow_soft_placement=True)
         config.gpu_options.allow_growth=True
-
+        
+        prev_val_rmse = np.inf
         with tf.Session(config=config) as sess:
+            self.training_handle = sess.run(self.train_iter.string_handle())
+            self.validation_handle = sess.run(self.val_iter.string_handle())
+
             sess.run(tf.local_variables_initializer())
             sess.run(tf.global_variables_initializer())
             logging.info("Initial variables have been initialized")
@@ -147,7 +135,7 @@ class Box_net():
             summary_writer = tf.summary.FileWriter(self.flags.logs,graph=tf.get_default_graph())
 
             sess.run(self.train_iter.initializer)
-            sess.run(self.val_iter.initializer)
+
             logging.info("TFData api has been initialized")
 
             val_counter = 0
@@ -156,7 +144,8 @@ class Box_net():
                     try:
                         if step % 10 == 0:
                             loss, step_count = self.train_step(sess,train_op, metrics_op, global_step)
-                            train_summaries = sess.run(train_summary_op)
+                            train_summaries = sess.run(train_summary_op,
+                                    feed_dict={self.training:True,self.handle:self.training_handle})
                             summary_writer.add_summary(train_summaries,step_count)
                         else:
                             loss,_ = self.train_step(sess,train_op,metrics_op,global_step)
@@ -167,20 +156,23 @@ class Box_net():
                         logging.info("Keyboard Interrupt")
                         sys.exit(0)
                 logging.info('Epoch %s/%s', epoch+1, self.flags.num_epochs)
-                learning_rate_value, rmse_value = sess.run([lr,rmse])
+                learning_rate_value, rmse_value = sess.run([lr,rmse],
+                    feed_dict={self.training:False,self.handle:self.training_handle})
                 logging.info('Current lr: %s', learning_rate_value)
                 logging.info('Current training rmse: %s',rmse_value)
 
                 logging.info("Validation")
                 v_loss = 0.0
                 count = 0
+                sess.run(self.val_iter.initializer)
                 while True:
                     val_counter += 1
                     count+=1
                     try:
-                        v_loss, _ = self.val_step(sess, val_loss, val_metrics_op)
+                        v_loss,_ = self.val_step(sess, total_loss, metrics_op)
                         if count % 10 == 0:
-                            val_summaries = sess.run(val_summary_op)
+                            val_summaries = sess.run(val_summary_op,
+                                feed_dict={self.training:False,self.handle:self.validation_handle})
                             summary_writer.add_summary(val_summaries,val_counter)
                         if count % 100 == 0:
                             logging.info('Processing validation sample # %s',count)
@@ -191,11 +183,12 @@ class Box_net():
                     except KeyboardInterrupt:
                         logging.info("Keyboard Interrupt")
                         sys.exit(0)
-                    except:
-                        count -= 1
-                        logging.info("There was a problem this iteration, skipping")
+#                    except :
+#                        count -= 1
+#                        logging.info("There was a problem this iteration, skipping")
 
-                v_rmse = sess.run([val_rmse])[0]
+                v_rmse = sess.run([rmse],feed_dict={self.training:False,
+                                                    self.handle:self.validation_handle})[0]
                 logging.info('Num validation samples processed: %s',count)
                 logging.info('Validation Loss: %s',v_loss)
                 logging.info('Validation RMSE: %s',v_rmse)
--- pipeline/resnet/classifier.py
+++ pipeline/resnet/classifier.py
@@ -30,22 +30,27 @@ class Classifier():
         self.train_dataset = data.get_batch(self.flags.data_dir)
         self.val_dataset = data.get_batch(self.flags.val_dir)
 
+        #create handle for switching iterators
+        self.handle = tf.placeholder(tf.string,shape=[])
+        self.iterator = tf.data.Iterator.from_string_handle(
+                self.handle, self.train_dataset.output_types,self.train_dataset.output_shapes)
+        self.image, self.label = self.iterator.get_next()
+
         self.train_iter = self.train_dataset.make_initializable_iterator()
         self.val_iter = self.val_dataset.make_initializable_iterator()
-       
-        self.train_image, self.train_label = self.train_iter.get_next()
-        self.val_image, self.val_label = self.val_iter.get_next()
 
     def train_step(self, sess, train_op, metrics_op, global_step):
         start_time = time.time()
-        total_loss, global_step_count, _ = sess.run([train_op, global_step, metrics_op])
+        total_loss, global_step_count, _ = sess.run([train_op, global_step, metrics_op],
+                feed_dict={self.training:True,self.handle:self.training_handle})
         time_elapsed = time.time() - start_time
         logging.info('global_step %s: loss %.4f (%.2f sec/step)', global_step_count, total_loss, time_elapsed)
         return total_loss, global_step_count
 
     def val_step(self, sess, val_op, val_metric):
         start_time = time.time()
-        v_loss, v_acc = sess.run([val_op, val_metric])
+        v_loss, v_acc = sess.run([val_op, val_metric],
+                feed_dict={self.training:False,self.handle:self.validation_handle})
         time_elapsed = time.time() - start_time
         return v_loss, v_acc
 
@@ -55,21 +60,18 @@ class Classifier():
         decay_steps = int(self.num_epochs_before_decay * num_steps_per_epoch)
 
         if self.dtype == 'float32':
-            self.train_image = tf.cast(self.train_image,tf.float32)
-            self.val_image = tf.cast(self.val_image,tf.float32)
-        else:
-            self.train_image = tf.cast(self.train_image,tf.float16)
-            self.val_image = tf.cast(self.val_image,tf.float16)
+            self.image = tf.cast(self.image,tf.float32)
 
-        train_logits, train_end_points = get_class_resnet(self.train_image,self.num_classes,is_training=True)
+        self.training = tf.placeholder(tf.bool,name="training")
+        logits, end_points = get_class_resnet(self.image,self.num_classes,is_training=self.training)
 
-        one_hot_labels = tf.one_hot(self.train_label, self.num_classes)
-        train_logits = tf.squeeze(train_logits,[1,2])
+        one_hot_labels = tf.one_hot(self.label, self.num_classes)
+        logits = tf.squeeze(logits,[1,2])
 
         if self.dtype == 'float16':
-            train_logits = tf.cast(train_logits, tf.float32)
+            logits = tf.cast(logits, tf.float32)
 
-        loss = tf.losses.softmax_cross_entropy(onehot_labels = one_hot_labels, logits = train_logits)
+        loss = tf.losses.softmax_cross_entropy(onehot_labels = one_hot_labels, logits = logits)
         
         loss = tf.losses.get_total_loss()
 
@@ -90,16 +92,18 @@ class Classifier():
         optimizer = tf.train.AdamOptimizer(learning_rate=lr)
         train_op = slim.learning.create_train_op(loss * loss_scale, optimizer, gradient_multipliers=grad_mult)
 
-        predictions = tf.squeeze(tf.argmax(train_end_points['predictions'],-1),[1])
-        probabilities = train_end_points['predictions']
 
-        accuracy, accuracy_update = tf.metrics.accuracy(self.train_label, predictions)
+
+        predictions = tf.squeeze(tf.argmax(end_points['predictions'],-1),[1])
+        probabilities = end_points['predictions']
+
+        accuracy, accuracy_update = tf.metrics.accuracy(self.label, predictions)
         metrics_op = tf.group(accuracy_update)
 
 
         #train summaries
-        train_image_summ = tf.summary.image('train/input_images',self.train_image[:5,:,:,:3])
-        train_mask_summ = tf.summary.image('train/input_masks',self.train_image[:5,:,:,3:])
+        train_image_summ = tf.summary.image('train/input_images',self.image[:5,:,:,:3])
+        train_mask_summ = tf.summary.image('train/input_masks',self.image[:5,:,:,3:])
         train_loss_summ = tf.summary.scalar('train/train_loss',loss)
         train_acc_summ = tf.summary.scalar('train/accuracy',accuracy)
         train_lr_summ = tf.summary.scalar('train/learning_rate',lr)
@@ -107,26 +111,10 @@ class Classifier():
         train_summary_op = tf.summary.merge([train_image_summ,
             train_mask_summ,train_loss_summ,train_acc_summ,train_lr_summ])
 
-        val_logits, val_end_points = get_class_resnet(self.val_image,self.num_classes,is_training=False)
-
-        val_one_hot_labels = tf.one_hot(self.val_label,self.num_classes)
-        val_logits = tf.squeeze(val_logits,[1,2])
-
-        if self.dtype=='float16':
-            val_logits = tf.cast(val_logits,tf.float32)
-
-        val_loss = tf.losses.softmax_cross_entropy(onehot_labels = one_hot_labels, logits=val_logits)
-
-        val_pred = tf.squeeze(tf.argmax(val_end_points['predictions'],-1),[1])
-        val_accuracy,val_accuracy_update = tf.metrics.accuracy(self.val_label, val_pred)
-        val_metrics_op = tf.group(val_accuracy_update)
-
-        
-        #summaries
-        val_image_summ = tf.summary.image('val/input_images',self.val_image[:5,:,:,:3])
-        val_mask_summ = tf.summary.image('val/input_masks',self.val_image[:5,:,:,3:])
-        val_loss_summ = tf.summary.scalar('val/train_loss',val_loss)
-        val_acc_summ = tf.summary.scalar('val/accuracy',val_accuracy)
+        val_image_summ = tf.summary.image('val/input_images',self.image[:5,:,:,:3])
+        val_mask_summ = tf.summary.image('val/input_masks',self.image[:5,:,:,3:])
+        val_loss_summ = tf.summary.scalar('val/train_loss',loss)
+        val_acc_summ = tf.summary.scalar('val/accuracy',accuracy)
 
         val_summary_op = tf.summary.merge([val_image_summ, val_mask_summ,
                                            val_loss_summ,val_acc_summ])
@@ -138,6 +126,9 @@ class Classifier():
         config.gpu_options.allow_growth=True
 
         with tf.Session(config=config) as sess:
+            self.training_handle = sess.run(self.train_iter.string_handle())
+            self.validation_handle = sess.run(self.val_iter.string_handle())
+
             sess.run(tf.local_variables_initializer())
             sess.run(tf.global_variables_initializer())
             logging.info("Initial variables have been initialized")
@@ -148,7 +139,6 @@ class Classifier():
             summary_writer = tf.summary.FileWriter(self.flags.logs,graph=tf.get_default_graph())
 
             sess.run(self.train_iter.initializer)
-            sess.run(self.val_iter.initializer)
             logging.info("TFData api has been initialized")
 
             val_counter = 0
@@ -157,7 +147,8 @@ class Classifier():
                     try:
                         if step % 10 == 0:
                             loss, step_count = self.train_step(sess,train_op, metrics_op, global_step)
-                            train_summaries = sess.run(train_summary_op)
+                            train_summaries = sess.run(train_summary_op,
+                                    feed_dict={self.training:True,self.handle:self.training_handle})
                             summary_writer.add_summary(train_summaries,step_count)
                         else:
                             loss,_ = self.train_step(sess,train_op,metrics_op,global_step)
@@ -168,20 +159,23 @@ class Classifier():
                         logging.info("Keyboard Interrupt")
                         sys.exit(0)
                 logging.info('Epoch %s/%s', epoch+1, self.flags.num_epochs)
-                learning_rate_value, accuracy_value = sess.run([lr,accuracy])
+                learning_rate_value, accuracy_value = sess.run([lr,accuracy],
+                        feed_dict={self.training:False,self.handle:self.training_handle})
                 logging.info('Current lr: %s', learning_rate_value)
                 logging.info('Current training accuracy: %s',accuracy_value)
 
                 logging.info("Validation")
                 v_loss = 0.0
                 count = 0
+                sess.run(self.val_iter.initializer)
                 while True:
                     val_counter += 1
                     count+=1
                     try:
-                        v_loss, _ = self.val_step(sess, val_loss, val_metrics_op)
+                        v_loss, _ = self.val_step(sess, loss, metrics_op)
                         if count % 10 == 0:
-                            val_summaries = sess.run(val_summary_op)
+                            val_summaries = sess.run(val_summary_op,
+                                    feed_dict={self.training:False,self.handle:self.validation_handle})
                             summary_writer.add_summary(val_summaries,val_counter)
                         if count % 100 == 0:
                             logging.info('Processing validation sample # %s',count)
@@ -192,11 +186,12 @@ class Classifier():
                     except KeyboardInterrupt:
                         logging.info("Keyboard Interrupt")
                         sys.exit(0)
-                    except:
-                        count -= 1
-                        logging.info("There was a problem this iteration, skipping")
+#                    except:
+#                        count -= 1
+#                        logging.info("There was a problem this iteration, skipping")
 
-                v_accuracy = sess.run([val_accuracy])[0]
+                v_accuracy = sess.run([accuracy],feed_dict={self.training:False,
+                                                            self.handle:self.validation_handle})[0]
                 logging.info('Num validation samples processed: %s',count)
                 logging.info('Validation Loss: %s',v_loss)
                 logging.info('Validation Accuracy: %s',v_accuracy)
